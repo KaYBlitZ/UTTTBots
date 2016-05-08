8c350bf398bcc495173ac947278f8eb14bd911b2
Kenneth Liang <kennethliang0@gmail.com>
2016-05-08 17:49:19 -0400

    Initial

---
+++ src/com/kayblitz/uttt/PlayerBot.java
@@ -0,0 +1,29 @@
+package com.kayblitz.uttt;
+
+import java.util.Scanner;
+
+public class PlayerBot {
+
+	public static void main(String[] args) {
+		new PlayerBot().run();
+	}
+
+	private Scanner scanner;
+
+	public PlayerBot() {
+		scanner = new Scanner(System.in);
+	}
+
+	public void run() {
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+
+			String[] parts = line.split(" ");
+			if (parts[0].equals("move")) { // player clicked on GUI, repeat move
+											// to game
+				System.out.println("place_move " + Integer.parseInt(parts[1]) + " " + Integer.parseInt(parts[2]));
+			}
+		}
+	}
+
+}
