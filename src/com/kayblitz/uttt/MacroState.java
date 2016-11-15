c9ab6b190dd5a50f7643e124672301a973398980
Kenneth Liang <kennethliang0@gmail.com>
2016-11-15 14:23:57 -0500

    Small optimizations

--- src/com/kayblitz/uttt/MacroState.java
+++ src/com/kayblitz/uttt/MacroState.java
@@ -1,30 +1,22 @@
 package com.kayblitz.uttt;
 
 public class MacroState {
-
-	int m00, m10, m20, m01, m11, m21, m02, m12, m22;
+	
+	private int[][] macroState;
+	
+	public MacroState() {
+		macroState = new int[3][3];
+	}
 	
 	public void saveState(int[][] macroField) {
-		m00 = macroField[0][0];
-		m10 = macroField[1][0];
-		m20 = macroField[2][0];
-		m01 = macroField[0][1];
-		m11 = macroField[1][1];
-		m21 = macroField[2][1];
-		m02 = macroField[0][2];
-		m12 = macroField[1][2];
-		m22 = macroField[2][2];
+		for (int i = 0; i < 3; i++) {
+			System.arraycopy(macroField[i], 0, macroState[i], 0, 3);
+		}
 	}
 	
 	public void restoreState(int[][] macroField) {
-		macroField[0][0] = m00;
-		macroField[1][0] = m10;
-		macroField[2][0] = m20;
-		macroField[0][1] = m01;
-		macroField[1][1] = m11;
-		macroField[2][1] = m21;
-		macroField[0][2] = m02;
-		macroField[1][2] = m12;
-		macroField[2][2] = m22;
+		for (int i = 0; i < 3; i++) {
+			System.arraycopy(macroState[i], 0, macroField[i], 0, 3);
+		}
 	}
 }
